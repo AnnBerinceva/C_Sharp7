dcb2cbd7ade07f45b4a2fac1dd986b29e6f368dd
AnnBerinceva <berinceva92@bk.ru>
2022-11-25 22:58:23 +0500

    Takk_27

---
+++ Takk_27/Program.cs
@@ -0,0 +1,28 @@
+﻿int num = ReadInt("Введите число ");
+int len = NumberLen(num);
+SumNumbers(num, len);
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int NumberLen(int x)
+{
+    int index = 0;
+    while (x > 0)
+    {
+        x /= 10; 
+        index++;
+    }
+    return index;
+}
+void SumNumbers(int b, int len)
+{
+    int sum = 0;
+    for (int a = 1; a <= len; a++)
+    {
+        sum += b % 10;
+        b /= 10;
+    }
+    Console.WriteLine(sum);
+}
