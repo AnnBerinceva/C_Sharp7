b26ffa67230e1557099eafe34fc3407341955bab
AnnBerinceva <berinceva92@bk.ru>
2022-11-25 22:57:41 +0500

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,12 @@
+﻿int lenArray = ReadInt("Длина массива ");
+int[] randomArray = new int[lenArray];
+for (int a = 0; a < randomArray.Length; a++)
+{
+    randomArray[a] = new Random().Next(1,9);
+    Console.Write(randomArray[a] + " ");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
