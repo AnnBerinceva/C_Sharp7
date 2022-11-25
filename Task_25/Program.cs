9c003c6b4228cee539842d70fe2b71d6f4e9c2e9
AnnBerinceva <berinceva92@bk.ru>
2022-11-25 22:56:53 +0500

    Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Число А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int x = 1; x < b; x++)
+{ 
+    step = step * a;
+}
+Console.WriteLine("A в натуральной степени B равно " + step); 
